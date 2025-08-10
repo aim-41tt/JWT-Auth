da76f0d0eb838c04e8163b734c228c3379dc581a
aim_41tt <0505mustik@gmail.com>
2025-08-10 21:56:29 +0300

    Fixing the JwtAuthenticationFilter.
    Used @Qualifier(cachedUserDetailsService) to get the desired UserDetailsService implementation.

--- src/main/java/ru/example/JWT_Auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/ru/example/JWT_Auth/filter/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,43 +22,44 @@ import java.io.IOException;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
+	private final JwtService jwtService;
+	private final UserDetailsService userDetailsService;
 
-    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
-        this.jwtService = jwtService;
-        this.userDetailsService = userDetailsService;
-    }
+	public JwtAuthenticationFilter(JwtService jwtService,
+			@Qualifier("cachedUserDetailsService") UserDetailsService userDetailsService) {
+		this.jwtService = jwtService;
+		this.userDetailsService = userDetailsService;
+	}
 
-    @Override
-    protected void doFilterInternal(
-    		@NonNull HttpServletRequest request,
-    		@NonNull HttpServletResponse response,
-    		@NonNull FilterChain filterChain)
-            throws ServletException, IOException {
+	@Override
+	protected void doFilterInternal(
+			@NonNull HttpServletRequest request, 
+			@NonNull HttpServletResponse response,
+			@NonNull FilterChain filterChain) throws ServletException, IOException {
 
-        final String authHeader = request.getHeader("Authorization");
-        final String jwt;
-        final String username;
+		final String authHeader = request.getHeader("Authorization");
+		final String jwt;
+		final String username;
 
-        // Проверка заголовка и извлечение токена
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
+		// Проверка заголовка и извлечение токена
+		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
 
-        jwt = authHeader.substring(7);
-        username = jwtService.extractUsername(jwt);
-        
-        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-        	UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-        	if(jwtService.isTokenValid(jwt, userDetails)) {
-        		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-        		authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-        		SecurityContextHolder.getContext().setAuthentication(authToken);
-        	}
-        }
+		jwt = authHeader.substring(7);
+		username = jwtService.extractUsername(jwt);
 
-        filterChain.doFilter(request, response);
-    }
+		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+			if (jwtService.isTokenValid(jwt, userDetails)) {
+				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
+						null, userDetails.getAuthorities());
+				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				SecurityContextHolder.getContext().setAuthentication(authToken);
+			}
+		}
+
+		filterChain.doFilter(request, response);
+	}
 }
