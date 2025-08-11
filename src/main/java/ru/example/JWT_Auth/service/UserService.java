e8b6949ec23397e1e729764e399c314a2c0e2ca4
aim_41tt <0505mustik@gmail.com>
2025-08-11 14:45:16 +0300

    Remove duplicate code in UserService
    Replaced custom method implementation with an existing one from the mapper to reduce redundancy and improve maintainability.

--- src/main/java/ru/example/JWT_Auth/service/UserService.java
+++ src/main/java/ru/example/JWT_Auth/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.example.JWT_Auth.DTO.UserDTO;
 import ru.example.JWT_Auth.DTO.request.UserUpdateRequest;
 import ru.example.JWT_Auth.DTO.request.resetPassword.ResetPassword;
+import ru.example.JWT_Auth.mapper.user.UserMapper;
 import ru.example.JWT_Auth.model.User;
 import ru.example.JWT_Auth.repository.UserRepository;
 import ru.example.JWT_Auth.service.cahe.UserCacheService;
@@ -16,7 +17,7 @@ import ru.example.JWT_Auth.service.confirmations.VerifiedService;
  * Сервис для управления пользователями.
  *
  * <p>
- * Содержит методы для получения и обновления профиля пользователя, а также для 
+ * Содержит методы для получения и обновления профиля пользователя, а также для
  * верификации email и сброса пароля.
  * </p>
  * 
@@ -30,21 +31,29 @@ public class UserService {
 	private final UserRepository userRepository;
 	private final VerifiedService verifiedService;
 	private final UserCacheService userCacheService;
+	private final UserMapper userMapper;
 
 	/**
 	 * Конструктор UserService — инициализирует сервис с репозиторием пользователей
 	 * и сервисом верификации email.
-	 * 
-	 * @param userCacheService сервис кеширывания пользователей.
-	 * @param userRepository  Репозиторий для работы с пользователями.
+	 * <p>
+	 * Создает экземпляр класса с указанными параметрами.
+	 * </p>
+	 *
+	 * @param userRepository Репозиторий для работы с пользователями.
 	 * @param verifiedService Сервис для верификации email.
+	 * @param userCacheService Сервис кеширывания пользователей.
+	 * @param userMapper Мапер пользователей.
+	 * 
 	 * @since 10.02.2025
+	 * @since 11.08.2025
 	 */
 	public UserService(UserRepository userRepository, VerifiedService verifiedService,
-			UserCacheService userCacheService) {
+			UserCacheService userCacheService, UserMapper userMapper) {
 		this.userRepository = userRepository;
 		this.verifiedService = verifiedService;
 		this.userCacheService = userCacheService;
+		this.userMapper = userMapper;
 	}
 
 	/**
@@ -74,7 +83,9 @@ public class UserService {
 	 * @since 10.02.2025
 	 */
 	@Transactional
-	public UserDTO updateUserProfile(String username, UserUpdateRequest updateRequest) throws UsernameNotFoundException {
+	@Deprecated
+	public UserDTO updateUserProfile(String username, UserUpdateRequest updateRequest)
+			throws UsernameNotFoundException {
 		User user = userRepository.findByUsername(username)
 				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
@@ -90,7 +101,7 @@ public class UserService {
 		}
 
 		User updatedUser = userRepository.save(user);
-		UserDTO userDTO= mapToDto(updatedUser);
+		UserDTO userDTO = userMapper.toUserDTO(updatedUser);
 		userCacheService.cacheUser(userDTO);
 		return userDTO;
 	}
@@ -112,23 +123,7 @@ public class UserService {
 	 * @param user Объект пользователя.
 	 * @since 10.02.2025
 	 */
-	public void resetPasswordUser(User user, ResetPassword resetPassword){
+	public void resetPasswordUser(User user, ResetPassword resetPassword) {
 		verifiedService.resetPasswordByUser(user, resetPassword);
-	}
-
-	/**
-	 * Метод mapToDto — Преобразует объект пользователя в DTO.
-	 *
-	 * @param user Объект пользователя.
-	 * @return Объект UserDTO с данными пользователя.
-	 * @since 10.02.2025
-	 */
-	private UserDTO mapToDto(User user) {
-		UserDTO dto = new UserDTO();
-		dto.setUsername(user.getUsername());
-		dto.setEmail(user.getEmail());
-		dto.setVerified(user.getVerified());
-		dto.setRole(user.getRole());
-		return dto;
 	}
 }
